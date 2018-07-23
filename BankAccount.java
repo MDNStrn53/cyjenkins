2a0cf8e85302dc8c37cc08da312f44a5a9d3fb8c
MDNStrn53 <40809676+MDNStrn53@users.noreply.github.com>
2018-07-23 18:36:10 -0400

    Create BankAccount.java

---
+++ BankAccount.java
@@ -0,0 +1,20 @@
+package ca.uwo.csd.cs2212.USERNAME;
+
+public class BankAccount {  #bank account class that maintained it balance
+
+  private double balance;   #The class maintained it balance
+
+  public BankAccount(double balance) { #Constructor that initialize balance
+    this.balance = balance;
+  }
+
+  public double debit(double amount) { #Debit method that allow us to withdraw
+
+    if (balance < amount) {
+      amount = balance;
+    }
+
+    balance -= amount; # Return amount that was actually debited from the acct
+    return amount;
+  }
+}
